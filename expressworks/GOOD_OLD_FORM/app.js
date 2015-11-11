b690ff3129da0a6ff3d553b4a95ab56a8035abe7
Jon Green <jgreen82@gatech.edu>
2015-11-11 14:58:50 -0500

    Solved Good Old Form by added a response to the form post method haha

--- expressworks/GOOD_OLD_FORM/app.js
+++ expressworks/GOOD_OLD_FORM/app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
 app.post('/form', function(req, res) {
-	console.log(req.body.str.split('').reverse().join(''));
+	res.end(req.body.str.split('').reverse().join(''));
 });
 
 app.get('/', function(req, res) {
